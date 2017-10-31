461d79d44ec3970258846cfeec6d4cabfcf3992f
Fremy Santana <frmysantana@yahoo.com>
2017-10-30 23:50:30 -0400

    Added Comments
    I also fixed the for loops and added a test array that runs all of the test cases.

--- SpinalTapCase.js
+++ SpinalTapCase.js
@@ -1,5 +1,4 @@
-/*Algorithm challenge on FreeCodeCamp.
-Convert a string to spinal case. Spinal case is 
+/*Convert a string to spinal case. Spinal case is 
 all-lowercase-words-joined-by-dashes.
 spinalCase("This Is Spinal Tap")    -->"this-is-spinal-tap"
 spinalCase("thisIsSpinalTap")       -->"this-is-spinal-tap"
@@ -12,41 +11,60 @@ function splitAtCaps(str) {
   var caps = new RegExp(/[A-Z]/, 'g');
   var capMatches = str.match(caps);
   var newStr = str;
+  
   if (capMatches) {
+  	// Return the same string if the only capital letter is the first letter
     if (capMatches.length === 1 && newStr.search(caps) === 0){
       return newStr;
     }
+    // Split at second capital letter, because the first capital letter is
+    // also the first letter.
     else if (newStr.search(caps) === 0) {
-      for (j = 1; j < capMatches.length; j++) {
-        newStr = newStr.replace(capMatches[j], ' ' + capMatches[j]);
+      for (var i = 1; i < capMatches.length; i++) {
+        newStr = newStr.replace(capMatches[i], ' ' + capMatches[i]);
       }
       return newStr.split(' ');
     }
+    // Split at all capital letters
     else {
-      for (j = 0; j < capMatches.length; j++) {
-        newStr = newStr.replace(capMatches[j], ' ' + capMatches[j]);
+      for (var i = 0; i < capMatches.length; i++) {
+        newStr = newStr.replace(capMatches[i], ' ' + capMatches[i]);
       }
       return newStr.split(' ');
     }
   }
+
+  // Return the same string if there were no capital letters.
   return newStr;
 }
 
 function spinalCase(str) {
+  // Split the string at all spaces, underscores and hyphens.
   var nonCaps = new RegExp(/[\s\_\-]/, 'g');
-
   var strArr = str.split(nonCaps);
-  for (i = 0; i < strArr.length; i++) {
+
+  // For the resulting substrings, split at any capital letters
+  for (var i = 0; i < strArr.length; i++) {
     var capsGone = splitAtCaps(strArr[i]);
     strArr[i] = capsGone;
   }
+
+  // Flatten the resulting nested array
   var newStrArr = strArr.reduce(function(a,b) {return a.concat(b);}, []);
 
   var spinalCase = [];
-  for (i=0; i < newStrArr.length; i++) {
+
+  // Adds the hyphens between each word and ensures all letters are lowercase
+  for (var i = 0; i < newStrArr.length; i++) {
     spinalCase.push(newStrArr[i].toLowerCase(), '-');
   }
-  spinalCase.pop();
+  spinalCase.pop(); // Remove the trailing hyphen
 
   return spinalCase.join('');
 }
+
+var testArr = ["This Is Spinal Tap","thisIsSpinalTap","The_Andy_Griffith_Show",
+				"Teletubbies say Eh-oh","AllThe-small Things"];
+
+// Run test cases.
+testArr.forEach(function(el) {console.log(spinalCase(el)); } );
