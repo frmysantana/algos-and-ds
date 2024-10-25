f9ebfa1273950e61415544ade1298b034c44f466
Fremy Santana <frmysantana@yahoo.com>
2024-10-25 19:11:56 -0400

    solution to neetcode problem 'merge two sorted linked lists'

---
+++ neetcode/mergeTwoSortedLinkedLists.js
@@ -0,0 +1,33 @@
+/**
+ * did not understand - study
+ */
+class Solution {
+    /**
+     * @param {ListNode} list1
+     * @param {ListNode} list2
+     * @return {ListNode}
+     */
+    mergeTwoLists(list1, list2) {
+        const dummy = { val: 0, next: null };
+        let node = dummy;
+
+        while (list1 && list2) {
+            if (list1.val < list2.val) {
+                node.next = list1;
+                list1 = list1.next;
+            } else {
+                node.next = list2;
+                list2 = list2.next;
+            }
+            node = node.next;
+        }
+
+        if (list1) {
+            node.next = list1;
+        } else {
+            node.next = list2;
+        }
+
+        return dummy.next;
+    }
+}
