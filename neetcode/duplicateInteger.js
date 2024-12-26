75763d5e925befdd3cc697ef58f554b248d4ee7e
Fremy Santana <frmysantana@yahoo.com>
2024-12-25 20:34:06 -0500

    2nd attempt at neetcode challenge 'contains duplicate' - though this solution isn't as good

--- neetcode/duplicateInteger.js
+++ neetcode/duplicateInteger.js
@@ -29,4 +29,25 @@ class Solution {
 
         return false
     }
+
+    /**
+     * sort first, then 2 pointers
+     * 
+     * time: O(n*log(n))
+     * space: O(n)
+     */
+    hasDuplicateV2(nums) {
+        const sortedNums = nums.sort((a, b) => a - b);
+        let second = 1;
+
+        for (let first = 0; first < nums.length - 1; first++) {
+            if (sortedNums[first] == sortedNums[second]) {
+                return true;
+            }
+
+            second++;
+        }
+
+        return false;
+    }
 }
